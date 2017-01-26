b25bc27c04c1a96e078d9ec3cc706f5214fd467b
duartemagalhaes <duarte.b.magalhaes@gmail.com>
2017-01-26 16:06:48 +0200

    Add files via upload

--- coal.f90
+++ coal.f90
@@ -1,12 +1,13 @@
 module numerical_util
   implicit none
+
   integer, parameter:: dp=kind(0.d0) !double precision
 end module   
 
 module abs_const
   use numerical_util
   implicit none
-  real (kind=dp) :: NA, R0
+  real (kind=dp) :: NA, R0,P,Mco,Mco2,Mn2,Mh2o,Mh2
   !
 contains
   !
@@ -14,6 +15,12 @@ contains
     open (unit=41, file ='abs_const.dat',form='formatted',status='old')
     read (41,*) NA
     read (41,*) R0
+    read (41,*) P
+    read (41,*) Mco
+    read (41,*) Mco2
+    read (41,*) Mn2
+    read (41,*) Mh2o
+    read (41,*) Mh2
     !not use unit 5 (keyboard) nor 6 (screen)
     close (41)
   end subroutine 
@@ -36,7 +43,6 @@ contains
     read (41,*) rho_char
     read (41,*) d_particle
     read (41,*) d_pore
-    !not use unit 5 (keyboard) nor 6 (screen)
     close (41)
   end subroutine 
 end module particle
@@ -52,7 +58,7 @@ contains
     open (unit=41, file ='surf.dat',form='formatted',status='old')
     read (41,*) E1bof
     read (41,*) E3
-    !not use unit 5 (keyboard) nor 6 (screen)
+    
     close (41)
   end subroutine read_surf_const
 end module surfchem
@@ -65,7 +71,7 @@ program coal
   implicit none
   !
   call read_abs_const()
-  write (7,*) NA,R0
+  write (7,*) NA,R0,P,Mco,Mco2,Mn2,Mh2o,Mh2
   call read_particle()
   write (8,*) tort,porosity,act_sites,total_sites,Mc,rho_char,d_particle,d_pore
   call read_surf_const()
